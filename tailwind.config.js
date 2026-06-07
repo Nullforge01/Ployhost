cd12683ed778545f24aa1c8c3f3cd7604abab0cf
Nullforge01 <tavongatoma100@gmail.com>
2026-06-07 22:52:51 +0200

    Update Tailwind CSS configuration for content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-  darkMode: 'class'
 }
